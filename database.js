b9538a9d9b821cd7e586c6971f51804de3048eb4
Jay Vipul Gala <gala.jay1@northeastern.edu>
2024-03-27 15:40:48 -0400

    Bug fixes

--- database.js
+++ database.js
@@ -1,4 +1,5 @@
 const mysql = require('mysql2');
+const strftime = require('strftime');
 
 const pool = mysql.createPool({
     host: process.env.MYSQL_HOST,
@@ -8,9 +9,16 @@ const pool = mysql.createPool({
 })
 .promise();
 
-export async function insertToken(user) {
+async function insertToken(user) {
     await pool.query(
-        'INSERT INTO email_token (user_id, token) VALUES (?, ?)',
-        [user.user_id, user.token]
+      "INSERT INTO email_token (user_id, token, created_at, updated_at) VALUES (?, ?, ?, ?)",
+      [
+        user.user_id,
+        user.token,
+        strftime("%Y-%m-%d %H:%M:%S"),
+        strftime("%Y-%m-%d %H:%M:%S"),
+      ]
     );
 }
+
+exports.insertToken = insertToken;
